06af9eb5699583e1bb8607a2c5ba76a9d47ff577
yardentamir <yarden.tamir@walla.co.il>
2023-12-21 21:39:50 +0200

    add validation of envs

--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,20 @@
-import dotenv from "dotenv";
+import cors from "cors";
+import "dotenv/config";
+import { cleanEnv, port, str } from "envalid";
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 
-dotenv.config();
+const { MONGODB_URL, PORT } = cleanEnv(process.env, {
+  MONGODB_URL: str(),
+  PORT: port(),
+});
 
 const app = express();
-const port = 3000;
 
-mongoose.connect(process.env.MONGODB_URL || "");
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(MONGODB_URL!);
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
@@ -19,6 +26,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World! MongoDB connection successful.");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 });
