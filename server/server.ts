08aa07111cd6a58cd651b5a918e5691660e93fc2
yardentamir <yarden.tamir@walla.co.il>
2023-12-21 20:43:05 +0200

    connect to mongodb

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,22 @@
+import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
-const port: number = 3000;
+const port = 3000;
+
+mongoose.connect(process.env.MONGODB_URL || "");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, World!");
+  res.send("Hello, World! MongoDB connection successful.");
 });
 
 app.listen(port, () => {
