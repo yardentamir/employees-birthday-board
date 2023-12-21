5ef69ad3bac2019a92c0bc3abdecb9c741d086e5
yardentamir <yarden.tamir@walla.co.il>
2023-12-21 20:03:21 +0200

    add docker to server

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 
 const app = express();
-const port: number = 8080;
+const port: number = 3000;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
