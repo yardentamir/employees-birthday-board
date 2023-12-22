bc4cb87e8809f80ba0b901a48a5ca184dbaf77b2
yardentamir <yarden.tamir@walla.co.il>
2023-12-22 09:00:13 +0200

    add 404 error message

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,8 @@
 import cors from "cors";
 import "dotenv/config";
 import { cleanEnv, port } from "envalid";
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 import "./database/mongodb";
 import Employee from "./models/employee.model";
 import employeeRoutes from "./routes/employee.route";
@@ -16,6 +17,10 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/employee", employeeRoutes);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  next(createHttpError(404, "Page Not Found"));
+});
 
 app.get("/", async (req: Request, res: Response) => {
   const employees = await Employee.find().exec();
